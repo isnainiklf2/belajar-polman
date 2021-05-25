846565dbec48a0f93576054b28cf4ce64cc98385
isnaini nur khalifah <isnainiklf16@gmail.com>
2021-05-25 14:27:01 +0700

    latihan untuk push github

--- belajar-polman/Program.cs
+++ belajar-polman/Program.cs
@@ -11,6 +11,7 @@ namespace belajar_polman
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// tes untuk push github
         [STAThread]
         static void Main()
         {
